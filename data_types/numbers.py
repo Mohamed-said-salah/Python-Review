fe4c7f736176c5cb1f6e00bfd2b77a495ce7c2de
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2026-02-08 02:10:23 +0200

    added gitignore file

--- data_types/numbers.py
+++ data_types/numbers.py
@@ -1,1 +1,14 @@
+# Basic Number Types
+# int
+# float
+# complex
+
+# int # all calculations are done in the background as float # int is a subclass of float # all calculations results are int except for the division operator /
+x = 10 
+
+# float
+y = 10.5
+
+# complex
+z = 4j
 
